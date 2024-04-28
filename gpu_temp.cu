2a6dd30b777ab0008d353e30d35458c7b7e7aa6b
Paul Hamin Suh <phs92@login19.chn.perlmutter.nersc.gov>
2024-04-28 14:17:05 -0700

    First implementation of parallelized split

--- gpu_temp.cu
+++ gpu_temp.cu
@@ -35,7 +35,7 @@ split_output_t split_serial(int D, int N, thrust::device_vector<double> &x_train
     int feature = std::numeric_limits<int>::infinity();
     double cut_value = std::numeric_limits<double>::infinity();
 
-    thrust::vector<split_output_t> splits (N * D);
+    thrust::vector<split_output_t> split_results ((N-1) * D);
 
     // iterate through each feature
     for (int d = 0; d < D; ++d)
@@ -60,29 +60,43 @@ split_output_t split_serial(int D, int N, thrust::device_vector<double> &x_train
         double mean_right = weight * thrust::reduce(y_train_sorted.begin(), y_train_sorted.end(), 0.0);
         double weight_right = 1.0;
 
-        thrust::vector<double> y_prefix_sum(N);
-        thrust::vector<double> y_squared_prefix_sum(N);
+        thrust::device_vector<double> y_prefix_sum(N);
+        thrust::device_vector<double> y_squared_prefix_sum(N);
 
         thrust::inclusive_scan(thrust::host, y_train_sorted.begin(), y_train_sorted.end(), y_prefix_sum.begin());
         thrust::inclusive_scan(thrust::host, y_train_sorted_squared.begin(), y_train_sorted_squared.begin(), y_squared_prefix_sum.begin());
 
-        // TODO: Also need to account for weight_left and weight_right. Can perform transform on array of indices (eg. 1 to N). Can also use custom GPU kernel.
-        thrust::transform(y_prefix_sum.begin(), y_prefix_sum.end(), y_squared_prefix_sum.begin(), losses.begin(), [weight, mean_square_right, mean_right] __device__ (double y, double y_squared) {
-                double mean_square_left = weight * y_squared;
-                double mean_left = weight * y;
+        thrust::device_vector<int> split_indices(N-1);
+        thrust::sequence(split_indices.begin(), split_indices.end());
+
+        thrust::transform(split_indices.begin(), split_indices.end(), losses.begin(), 
+                [N, d, split_results, weight, mean_square_right, mean_right, y_prefix_sum, y_squared_prefix_sum, x_train_sorted] __device__ (int index) {
+                double mean_square_left = weight * y_squared_prefix_sum[index];
+                double mean_left = weight * y_prefix_sum[index];
                 double local_mean_right = mean_right - mean_left; 
                 double local_mean_sq_right = mean_square_right - mean_square_left;
+                double weight_left = (index + 1) * weight;
+                double weight_right = (N - index - 1) * weight;
                 double left_loss = mean_square_left - pow(mean_left, 2) / weight_left;
                 double right_loss = local_mean_sq_right - pow(local_mean_right, 2) / weight_right;
-                double loss = left_loss + right_loss;
+                split_output_t split_output;
+                split_output.cut_feature = d;
+                split_output.cut_value = (x_train_sorted[index] + x_train_sorted[index + 1]) / 2;
+                split_output.loss = left_loss + right_loss;
+                split_results[(N-1)*d + index] = split_output;
             });
 
+        cudaDeviceSynchronize();
     }
 
-    assert(feature != std::numeric_limits<int>::infinity());
+    split_output_t output = thrust::reduce(thrust::device, split_results.begin(), split_results.end(), split_results[0], 
+            [] __device__ (split_output_t& left, split_output_t& right) {
+            if (left.loss < right.loss) {
+                return left;
+            } else {
+                return right;
+            }
+        });
 
-    split_output_t output;
-    output.cut_feature = feature;
-    output.cut_value = cut_value;
     return output;
 }
